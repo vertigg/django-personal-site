45930a3dda273aa8420523c883af9f55fb800cc4
EPICWIN\EpicVertigo <vertigo.spy@gmail.com>
2018-05-24 13:29:14 +0300

    Fix url pattern for poe leagues with parenthesis

--- poeladder/urls.py
+++ poeladder/urls.py
@@ -4,5 +4,5 @@ import poeladder.views as views
 urlpatterns = [
     url(r'^$', views.ladder, name='ladder_main'),
     url(r'^search/$', views.search, name="ladder_search"),
-    url(r'^(?P<league>[\w\-]+)/$', views.league_ladder, name='ladder_url'),
+    url(r'^(?P<league>[\w\-\S]+)/$', views.league_ladder, name='ladder_url'),
 ]
