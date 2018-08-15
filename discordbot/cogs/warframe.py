438d474225528f76a3eb099a50ff6eeacc08a38f
EPICWIN\EpicVertigo <vertigo.spy@gmail.com>
2018-08-15 11:00:15 +0300

    Change querysets for WarframeWatchdog

--- discordbot/cogs/warframe.py
+++ discordbot/cogs/warframe.py
@@ -9,12 +9,13 @@ import discord
 from discord.errors import InvalidArgument
 from discordbot.models import DiscordUser, WFAlert
 from discord.utils import get as get_user
+from django.db.models import Q
 
 logger = logging.getLogger("botLogger.warframe")
 
 
 class Warframe(object):
-    
+
     def __init__(self, bot):
         self.bot = bot
         self.watchdog = self.bot.loop.create_task(self.warframe_alert_watchdog())
@@ -55,15 +56,11 @@ class Warframe(object):
                     matches = [self.RESOURCES[k] for k in keywords if k in self.RESOURCES]
                     if matches:
                         if len(matches) == 2:
-                            query1 = DiscordUser.objects.select_related().filter(
-                                **{'wf_settings__{}'.format(matches[0]): True})
-                            query2 = DiscordUser.objects.select_related().filter(
-                                **{'wf_settings__{}'.format(matches[1]): True})
-                            subscribers = query1.union(query2)
+                            subscribers = DiscordUser.objects.select_related().filter(
+                                Q(**{'wf_settings__{}'.format(matches[0]): True}) | Q(**{'wf_settings__{}'.format(matches[1]): True}))
                         else:
-                            query_filter = {
-                                'wf_settings__{}'.format(matches[0]): True}
-                            subscribers = DiscordUser.objects.select_related().filter(**query_filter)
+                            subscribers = DiscordUser.objects.select_related().filter(
+                                **{'wf_settings__{}'.format(matches[0]): True})
                         for sub in subscribers:
                             try:
                                 user = get_user(self.bot.get_all_members(), id=sub.id)
@@ -78,12 +75,13 @@ class Warframe(object):
             await asyncio.sleep(60)
 
     def create_embed(self, alert):
-        embed = discord.Embed(title="**Warframe Alert**", 
+        embed = discord.Embed(title="**Warframe Alert**",
                               colour=discord.Colour(0xff0074),
                               description="{}\n\n[Unsubscribe](https://epicvertigo.xyz/profile)".format(alert.content))
         embed.set_thumbnail(url="https://i.imgur.com/ZvDNumd.png")
         embed.set_footer(text="WFAlert ID: {}".format(alert.id))
         return embed
+
 
 def setup(bot):
     bot.add_cog(Warframe(bot))
