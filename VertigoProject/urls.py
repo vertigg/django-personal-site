9cea5b68d70200bd1e42c1d35b69f1fc04519c3e
EpicVertigo <vertigo.spy@gmail.com>
2019-01-06 14:59:11 +0200

    Remove wowstats app

--- VertigoProject/urls.py
+++ VertigoProject/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     url(r'^games/', include('unitygames.urls')),
     url(r'^ladder/', include('poeladder.urls')),
     url(r'^discordbot/', include('discordbot.urls')),
-    url(r'^wow/', include('wowstats.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'admin/', admin.site.urls),
 ]
