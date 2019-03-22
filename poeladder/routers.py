9e2c0a1414058feb0bb5440a45f241eb64b905bf
EpicVertigo <vertigo.spy@gmail.com>
2019-03-22 22:12:06 +0200

    Remove unused assets, fix ladder redirect error

--- poeladder/routers.py
+++ poeladder/routers.py
@@ -3,9 +3,5 @@ from rest_framework.routers import APIRootView
 from rest_framework import routers
 
 
-class LadderTestAPIView(APIRootView):
-    pass
-
-
 class LadderRouter(routers.DefaultRouter):
     APIRootView = LadderTestAPIView
