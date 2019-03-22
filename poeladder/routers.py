eaf7fd4189b71664403647b039fc00aaee46235b
EpicVertigo <vertigo.spy@gmail.com>
2019-03-22 22:15:05 +0200

    Fix LadderRouter missing reference

--- poeladder/routers.py
+++ poeladder/routers.py
@@ -3,5 +3,9 @@ from rest_framework.routers import APIRootView
 from rest_framework import routers
 
 
+class LadderTestAPIView(APIRootView):
+    pass
+
+
 class LadderRouter(routers.DefaultRouter):
     APIRootView = LadderTestAPIView
