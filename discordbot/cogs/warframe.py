67d60c64f8b5b382616d9102d09061d5902da213
Vertig <vertigo.spy@gmail.com>
2019-08-09 02:32:20 +0300

    Use tasks api for Warframe watchdog

--- discordbot/cogs/warframe.py
+++ discordbot/cogs/warframe.py
@@ -2,12 +2,11 @@
 Cog for checking Warframe alerts
 """
 
-import asyncio
 import logging
 
 import discord
 from discord.errors import InvalidArgument
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.utils import get as get_user
 from django.db.models import Q
 
@@ -20,8 +19,7 @@ class Warframe(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.watchdog = self.bot.loop.create_task(
-            self.warframe_alert_watchdog())
+        self.watchdog = self.warframe_alert_watchdog.start()
         self.RESOURCES = {
             'Nitain Extract (Resource)': 'nitain_extract',
             'Orokin Cell (Resource)': 'orokin_cell',
@@ -46,41 +44,42 @@ class Warframe(commands.Cog):
         }
 
     def cog_unload(self):
-        if self.watchdog:
-            self.watchdog.cancel()
+        self.watchdog.cancel()
 
+    @tasks.loop(seconds=60, reconnect=True)
     async def warframe_alert_watchdog(self):
+        new_alerts = WFAlert.objects.filter(announced=False)
+        for alert in new_alerts:
+            if alert.keywords:
+                keywords = alert.keywords.split(',')
+                matches = [self.RESOURCES[k]
+                           for k in keywords if k in self.RESOURCES]
+                if matches:
+                    if len(matches) == 2:
+                        subscribers = DiscordUser.objects.select_related().filter(
+                            Q(**{f'wf_settings__{matches[0]}': True}) |
+                            Q(**{f'wf_settings__{matches[1]}': True}))
+                    else:
+                        subscribers = DiscordUser.objects.select_related().filter(
+                            **{f'wf_settings__{matches[0]}': True})
+                    for sub in subscribers:
+                        try:
+                            user = get_user(
+                                self.bot.get_all_members(), id=sub.id)
+                            if user:
+                                await user.send(embed=self.create_embed(alert))
+                            else:
+                                logger.error(
+                                    "Can't find %s in get_all_members(). User unsubbed", sub)
+                                self._unsub_user(sub)
+                        except InvalidArgument:
+                            pass
+            alert.announced = True
+            alert.save()
+
+    @warframe_alert_watchdog.before_loop
+    async def before_task(self):
         await self.bot.wait_until_ready()
-        while not self.bot.is_closed():
-            new_alerts = WFAlert.objects.filter(announced=False)
-            for alert in new_alerts:
-                if alert.keywords:
-                    keywords = alert.keywords.split(',')
-                    matches = [self.RESOURCES[k]
-                               for k in keywords if k in self.RESOURCES]
-                    if matches:
-                        if len(matches) == 2:
-                            subscribers = DiscordUser.objects.select_related().filter(
-                                Q(**{f'wf_settings__{matches[0]}': True}) |
-                                Q(**{f'wf_settings__{matches[1]}': True}))
-                        else:
-                            subscribers = DiscordUser.objects.select_related().filter(
-                                **{f'wf_settings__{matches[0]}': True})
-                        for sub in subscribers:
-                            try:
-                                user = get_user(
-                                    self.bot.get_all_members(), id=sub.id)
-                                if user:
-                                    await user.send(embed=self.create_embed(alert))
-                                else:
-                                    logger.error(
-                                        "Can't find %s in get_all_members(). User unsubbed", sub)
-                                    self._unsub_user(sub)
-                            except InvalidArgument:
-                                pass
-                alert.announced = True
-                alert.save()
-            await asyncio.sleep(60)
 
     def create_embed(self, alert):
         embed = discord.Embed(
