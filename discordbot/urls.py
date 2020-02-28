1898ef223977771786c9eb8f76e5305bbd66790f
Vertig <vertigo.spy@gmail.com>
2020-02-28 16:38:19 +0200

    Add corona endpoint

--- discordbot/urls.py
+++ discordbot/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from discordbot.views import WarframeWebhookView
+from discordbot.views import WarframeWebhookView, corona_report
+
+app_name = 'discordbot'
 
 urlpatterns = [
     path('warframe-webhook', WarframeWebhookView.as_view(), name='warframe_webhook'),
+    path('corona-report', corona_report, name='corona_report'),
 ]
--- discordbot/views.py
+++ discordbot/views.py
@@ -1,13 +1,35 @@
 import json
 
+import pandas as pd
 from django.conf import settings
 from django.http import JsonResponse
-from django.shortcuts import Http404
+from django.shortcuts import Http404, render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
-from discordbot.models import WFAlert
+from discordbot.models import CoronaReport, WFAlert
+
+
+def corona_report(request):
+    qs = (CoronaReport.objects
+          .filter(timestamp__hour=0)
+          .values('timestamp', 'deaths', 'recovered', 'confirmed')
+          .order_by('timestamp')
+          )
+    df = pd.DataFrame.from_dict(qs)
+    categories = list(df.timestamp.dt.strftime('%Y-%m-%d'))
+    deaths = list(df.deaths)
+    recovered = list(df.recovered)
+    confirmed = list(df.confirmed)
+
+    context = {
+        "categories": categories,
+        'recovered': recovered,
+        'deaths': deaths,
+        'confirmed': confirmed
+    }
+    return render(request, 'coronareport.html', context=context)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
