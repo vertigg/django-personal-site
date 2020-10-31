1e2c33850441d7380a73b00228294c9d85e85346
Vertig <vertigo.spy@gmail.com>
2020-10-31 13:15:15 +0000

    Add test deployment script

---
+++ .devcontainer/deployment.py
@@ -0,0 +1,18 @@
+import os
+import paramiko
+
+USERNAME = os.getenv('SSH_USERNAME')
+PASSWORD = os.getenv('SSH_PASSWORD')
+SERVER_ADDRESS = os.getenv('SERVER_ADDRESS')
+PATH = os.getenv('PRODUCTION_PROJECT_PATH')
+
+if not all(USERNAME, PASSWORD, PATH, SERVER_ADDRESS):
+    raise Exception('Script is improperly configured')
+
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(SERVER_ADDRESS, username=USERNAME, password=PASSWORD, allow_agent=False)
+
+command = f'cd {PATH} && git pull && supervisorctl restart homesite && supervisortcl restart bot'
+stdin, stdout, stderr = ssh_client.exec_command(command)
+ssh_client.close()
