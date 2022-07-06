b5651c1e1699cf3bebd1a7bb49842d65177b6ae7
Vertig <vertigo.spy@gmail.com>
2022-07-06 14:58:12 +0300

    Convert blacklist generation into Celery task

--- main/tasks.py
+++ main/tasks.py
@@ -1,14 +1,20 @@
+import logging
 import random
 import re
 from datetime import datetime, timedelta
 
 import requests
+from celery import Task
+from config.celery import register_task
 from django.contrib.staticfiles.storage import staticfiles_storage
-from main.management.commands.utils import AdvancedCommand
 
 
-class Command(AdvancedCommand):
-    help = 'Generates blacklist IP for Rion'
+logger = logging.getLogger(__name__)
+
+
+@register_task
+class GenerateBlacklistTask(Task):
+    """Generates blacklist IP for Rion"""
     padding = 40
     separator = '#' * padding
     pattern = re.compile(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(?:\/[0-9]{1,2})?')
@@ -31,10 +37,9 @@ class Command(AdvancedCommand):
             f'{self.random_title} IP list',
             f'Fetched {datetime.now()}',
             f'Next {datetime.now() + timedelta(days=1)}',
-            f'Generated in {self.execution_time} second(s)',
         ]))
 
-    def handle(self, *args, **options):
+    def run(self, *args, **options):
         ip_list = []
         for url in self.urls:
             try:
@@ -43,11 +48,10 @@ class Command(AdvancedCommand):
                     raise Exception(f'Response returned with status {response.status_code}')
                 ip_list.extend(self.pattern.findall(response.text))
             except Exception as exc:
-                self.logger.error(
+                logger.error(
                     'Could not fetch IP data from %s. Error details: %s', url, exc
                 )
         ip_list = sorted(set(ip_list))
         with open(self.filepath, 'w', encoding='utf-8') as f:
             f.writelines('\n'.join([self.separator, self.generate_header(), self.separator]))
             f.writelines([f'\n{address}' for address in ip_list])
-        self.log_execution_time()
