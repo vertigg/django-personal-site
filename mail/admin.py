056613fc131d2e27f7e4cad12365a1284eec1f58
Sergiy Salnikov <vertigo.spy@gmail.com>
2022-07-12 13:56:29 +0300

    Add mailbox filter for email messages

--- mail/admin.py
+++ mail/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from mail.models import Mailbox, EmailMessage
 
 admin.site.register(Mailbox)
-admin.site.register(EmailMessage)
+
+
+@admin.register(EmailMessage)
+class EmailMessageAdmin(admin.ModelAdmin):
+    list_filter = ('mailbox', )
+    search_fields = ('subject', )
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in self.model._meta.fields]
