12dc2844b11fa68a262184fe6418301117433e95
Sergiy Salnikov <vertigo.spy@gmail.com>
2022-11-24 12:13:34 +0200

    Rename poeladder app

--- poeladder/static/poeladder/js/ext.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  if (releaseDate > new Date()) {
-    $("#timer").countdown({ until: releaseDate, padZeroes: true, labels: [] });
-  } else {
-    $("#timer").html("League released!");
-  }
-};
